1411096c34a2bb9c71b6b3d233224bab0a8b9dc6
elgato-Nya <afiq.sharifuzan02@gmail.com>
2025-08-18 17:47:15 +0800

    refactor(user): refactor address controller to address service to make it leaner

---
+++ server/controllers/user/address.controller.js
@@ -0,0 +1,93 @@
+const BaseController = require("../base.controller");
+const { address: addressService } = require("../../services/user");
+const asyncHandler = require("../../utils/asyncHandler");
+const { sanitizeObject } = require("../../utils/sanitizer");
+
+const baseController = new BaseController();
+
+const getAddresses = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const addresses = await addressService.getUserAddresses(userId);
+
+  return baseController.sendSuccess(
+    res,
+    addresses,
+    "Addresses retrieved successfully"
+  );
+}, "get_addresses");
+
+const addAddress = asyncHandler(async (req, res) => {
+  const addressDTO = {
+    userId: req.user._id,
+    addressData: req.body,
+  };
+
+  const sanitizedAddress = sanitizeObject(addressDTO.addressData);
+
+  const newAddress = await addressService.addUserAddress(
+    addressDTO.userId,
+    sanitizedAddress
+  );
+
+  return baseController.sendSuccess(
+    res,
+    newAddress,
+    "Address added successfully"
+  );
+}, "add_address");
+
+const updateAddress = asyncHandler(async (req, res) => {
+  const addressDTO = {
+    userId: req.user._id,
+    addressId: req.params?.id,
+    addressData: req.body,
+  };
+
+  const sanitizedAddress = sanitizeObject(addressDTO.addressData);
+
+  const updatedAddress = await addressService.updateUserAddress(
+    addressDTO.userId,
+    addressDTO.addressId,
+    sanitizedAddress
+  );
+
+  return baseController.sendSuccess(
+    res,
+    updatedAddress,
+    "Address updated successfully"
+  );
+}, "update_address");
+
+const deleteAddress = asyncHandler(async (req, res) => {
+  const addressDTO = {
+    userId: req.user._id,
+    addressId: req.params?.id,
+  };
+
+  const data = await addressService.deleteUserAddress(
+    addressDTO.userId,
+    addressDTO.addressId
+  );
+
+  return baseController.sendSuccess(res, data, "Address deleted successfully");
+}, "delete_address");
+
+const getDefaultAddress = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const type = req.query?.type;
+  const address = await addressService.getDefaultAddress(userId, type);
+
+  return baseController.sendSuccess(
+    res,
+    address,
+    "Default address retrieved successfully"
+  );
+}, "get_default_address");
+
+module.exports = {
+  getAddresses,
+  addAddress,
+  updateAddress,
+  deleteAddress,
+  getDefaultAddress,
+};
--- server/services/user/address.service.js
+++ server/services/user/address.service.js
@@ -1,5 +1,5 @@
 const User = require("../../models/user");
-const { handleServiceError } = require("../base.service");
+const { handleServiceError, handleNotFoundError } = require("../base.service");
 const logger = require("../../utils/logger");
 const { createNotFoundError, AppError } = require("../../utils/errors");
 const { sanitizeObject } = require("../../utils/sanitizer");
@@ -8,8 +8,13 @@ const getUserAddresses = async (userId) => {
   try {
     const user = await User.findById(userId).select("addresses");
     if (!user) {
-      throw createNotFoundError("User", "USER_NOT_FOUND");
+      handleNotFoundError("User", "USER_NOT_FOUND", "get_user_addresses", {
+        userId,
+      });
     }
+
+    await user.updateLastActive();
+
     return user.addresses || [];
   } catch (error) {
     handleServiceError(error, "getUserAddresses", { userId });
@@ -20,14 +25,21 @@ const addUserAddress = async (userId, addressData) => {
   try {
     const user = await User.findById(userId).select("addresses");
     if (!user) {
-      throw createNotFoundError("User", "USER_NOT_FOUND");
+      handleNotFoundError("User", "USER_NOT_FOUND", "add_user_address", {
+        userId,
+      });
     }
 
-    if (user.addresses?.length >= 5) {
+    const type = addressData.type;
+    const maxPerType = 5; // Set your desired limit
+    const countOfType = user.addresses.filter(
+      (addr) => addr.type === type
+    ).length;
+    if (countOfType >= maxPerType) {
       throw new AppError(
-        "Maximum 5 addresses allowed per user",
+        `Maximum ${maxPerType} addresses allowed for type '${type}'`,
         400,
-        "ADDRESS_LIMIT_EXCEEDED"
+        "ADDRESS_TYPE_LIMIT_EXCEEDED"
       );
     }
 
@@ -40,14 +52,17 @@ const addUserAddress = async (userId, addressData) => {
     }
 
     user.addresses.push(addressData);
+    user.lastActive = new Date();
+    user.isActive = true;
     await user.save();
 
     const newAddress = user.addresses[user.addresses.length - 1];
     logger.info(`New address added for username: ${user.username}`, {
       userId,
       addressId: newAddress._id,
-      addressType: addressData.type,
+      addressType: newAddress.type,
     });
+    return newAddress;
   } catch (error) {
     handleServiceError(error, "addUserAddress", { userId, addressData });
   }
@@ -55,7 +70,6 @@ const addUserAddress = async (userId, addressData) => {
 
 const updateUserAddress = async (userId, addressId, updateData) => {
   try {
-    const sanitizedAddressData = sanitizeObject(updateData);
     const user = await User.findById(userId).select("addresses");
     if (!user) {
       throw createNotFoundError("User", "USER_NOT_FOUND");
@@ -66,7 +80,7 @@ const updateUserAddress = async (userId, addressId, updateData) => {
       throw createNotFoundError("Address", "ADDRESS_NOT_FOUND");
     }
 
-    if (sanitizedAddressData.isDefault) {
+    if (updateData.isDefault) {
       user.addresses.forEach((addr) => {
         if (addr.type === address.type && addr._id.toString() !== addressId) {
           addr.isDefault = false;
@@ -75,7 +89,7 @@ const updateUserAddress = async (userId, addressId, updateData) => {
     }
 
     // the updateData will overwrite the existing address fields
-    Object.assign(address, sanitizedAddressData);
+    Object.assign(address, updateData);
     await user.save();
 
     logger.info("Address updated", { userId, addressId });
@@ -104,10 +118,10 @@ const deleteUserAddress = async (userId, addressId) => {
     // TODO: Business rule: Don't allow deletion of the last address if there are active orders
 
     user.addresses.pull(addressId);
-    await user.save();
+    await user.save({ validateBeforeSave: false });
 
     logger.info("Address deleted", { userId, addressId });
-    return 0;
+    return { userId, addressId };
   } catch (error) {
     handleServiceError(error, "deleteUserAddress", { userId, addressId });
   }

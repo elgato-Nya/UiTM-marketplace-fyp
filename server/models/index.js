488bce1b5b39be36d11278c5568441cc0e187a91
elgato-Nya <afiq.sharifuzan02@gmail.com>
2025-12-01 13:20:51 +0800

    refactor(model): consolidate model exports
    - Add centralized model export
    - Configure model imports

---
+++ server/models/index.js
@@ -0,0 +1,44 @@
+const { User, Address, Merchant } = require("./user");
+const Listing = require("./listing/listing.model");
+const {
+  Order,
+  buyerInfoSchema,
+  sellerInfoSchema,
+  orderItemSchema,
+  deliveryAddressSchema,
+} = require("./order");
+const { Cart, cartItemSchema } = require("./cart/cart.model");
+const { Wishlist } = require("./wishlist/wishlist.model");
+const { CheckoutSession } = require("./checkout");
+const { MerchantAnalytics, PlatformAnalytics } = require("./analytic");
+
+module.exports = {
+  // User exports
+  User,
+  Address,
+  Merchant,
+
+  // Listing exports
+  Listing,
+
+  // Order exports
+  Order,
+  buyerInfoSchema,
+  sellerInfoSchema,
+  orderItemSchema,
+  deliveryAddressSchema,
+
+  // Cart exports
+  Cart,
+  cartItemSchema,
+
+  // Wishlist exports
+  Wishlist,
+
+  // Checkout exports
+  CheckoutSession,
+
+  // Analytics exports
+  MerchantAnalytics,
+  PlatformAnalytics,
+};

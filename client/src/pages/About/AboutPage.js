4d3c76f0164e46e12aab44883f02aed4df615937
elgato-Nya <afiq.sharifuzan02@gmail.com>
2025-12-03 16:35:41 +0800

    feat(page): add hash navigation to about page
    - add section IDs for #about and #history
    - implement smooth scrolling to sections
    - support hash-based navigation from menu

--- client/src/pages/About/AboutPage.js
+++ client/src/pages/About/AboutPage.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@mui/material";
+import { useLocation } from "react-router-dom";
 
 // Import about components
 import AboutHero from "../../components/about/AboutHero";
@@ -13,13 +14,29 @@ import AcademicProject from "../../components/about/AcademicProject";
 import CallToAction from "../../components/about/CallToAction";
 
 function AboutPage() {
+  const location = useLocation();
+
+  // Handle smooth scrolling to hash sections
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.querySelector(location.hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    }
+  }, [location]);
+
   return (
     <Box sx={{ width: "100%", pb: 6 }}>
       {/* Hero Section */}
-      <AboutHero />
+      <Box id="about">
+        <AboutHero />
+      </Box>
 
       {/* Our Story - The beginning and history */}
-      <OurStory />
+      <Box id="history">
+        <OurStory />
+      </Box>
 
       {/* Our Vision - Future goals */}
       <OurVision />
